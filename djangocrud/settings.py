704ff6d98acee4054e5090aa4638c8313e9e5390
Javier Henao <javier.henao@outlook.com>
2026-02-08 20:41:01 -0500

    Actualizacion whitenoise

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -55,6 +55,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    
 ]
 
 ROOT_URLCONF = 'djangocrud.urls'
@@ -133,6 +136,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ALLOWED_HOSTS = ["localhost", "djangocrud-production.up.railway.app"] 
 
